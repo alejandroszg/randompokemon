fe907ecf28ec737e702938c46df8125116bb7dc0
Alejandro Sanchez <alejandroszg@gmail.com>
2020-06-30 17:19:47 -0400

    version estable, detalles css

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
 const boton = document.getElementById('boton');
-const listaPokemon = document.getElementById('tipo-pokemon')
 
-//Agregar tipos de pokemones a la lista
 boton.addEventListener('click', async (e) => {
     e.preventDefault();
-    await fetch('https://pokeapi.co/api/v2/pokemon-form/?limit=151')
-    .then( data =>  data.json() )
-    .then( data =>  imprimirPokemon(data.results));
-})
-
-function imprimirPokemon(pokemon){
-    //Genera ID random de pokemon
+    //Generate random pokemon ID
     const idPokemon = parseInt(Math.random() * 151)+1;
-    //Almacena nombre en variable
-    const nombrePokemon = pokemon[idPokemon-1].name;
+    //Call to the API
+    await fetch('https://pokeapi.co/api/v2/pokemon-form/?limit=151')
+    .then(data => data.json())
+    .then(pokemones => {
+        const listaPokemones = pokemones.results;
+        imprimirPokemones(listaPokemones, idPokemon)})});
+        
+async function imprimirPokemones (pokemones, idPokemones){
+    const nombrePokemon = pokemones[idPokemones-1].name;
+    const tiposPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${idPokemones}/`);
+    const jsonPokemon = await tiposPokemon.json();
+    const tipoPokemon = jsonPokemon.types[0].type.name;
+    
     const div = document.getElementById('poke-tarjeta');
-    if(div.value!==''){
-        div.innerHTML += 
-            `
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${idPokemon}.png" class="foto" alt="">
-            <p class="poke-number">${idPokemon}</p>
-            <p class="poke-tittle">${nombrePokemon}</p>
-            <p class="poke-type">Agua</p>
-            `;
-            const contenedorPokemon = document.getElementById('div-pokemon');
-            contenedorPokemon.appendChild(div);
-    }else{
-        console.log('tiene algo')
-    }
+    div.innerHTML = 
+    `
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${idPokemones}.png" class="foto" alt="">
+        <p class="poke-number"><b>Id:</b> ${idPokemones}</p>
+        <p class="poke-tittle titulo"><b>Pokemon:</b> ${nombrePokemon}</p>
+        <p class="poke-type titulo"><b>Type:</b> ${tipoPokemon}</p>
+    `
+    const contenedorPokemon = document.getElementById('div-pokemon');
+    contenedorPokemon.appendChild(div);
 }
