69cdb30d0f380bfaeaa12c62a796e9582b1f4abf
Alejandro Sanchez <alejandroszg@gmail.com>
2023-06-06 20:28:45 -0400

    fix: saving the API on the document load
    style: updating 'generate random pokemon' button

--- app.js
+++ app.js
@@ -1,73 +1,124 @@
 const boton = document.getElementById('boton');
 
-boton.addEventListener('click', async (e) => {
+let pokedex = [];
+let pokePhoto = [];
+let pokeType = [];
+let _pokeColor = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://pokeapi.co/api/v2/pokemon-form/?limit=151')
+        .then((response) => {
+            if(response.ok) {
+                return response.json();
+            }
+            throw new Error('Error en la conexión a la API');
+        })
+        .then((data) => {
+            pokedex = data.results;
+            const promises = pokedex.map((form) =>
+                fetch(form.url).then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                    throw new Error('Error en la conexión a la API');
+                })
+            );
+            return Promise.all(promises);
+        })
+        .then((pokemonData) => {
+            const sprites = pokemonData.map((pokemon) => pokemon.sprites.front_default);
+            pokePhoto = sprites;
+            const tipo = pokemonData.map((pokemon) => pokemon.types[0].type.name)
+            pokeType = tipo;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    fetch('https://pokeapi.co/api/v2/pokemon-species/?limit=151')
+        .then((resp) => {
+            if(resp.ok) {
+                return  resp.json();
+            }
+            throw new Error('Error en la conexión a la API')
+        })
+        .then((info) => {
+            pokeSpecies = info.results;
+            const promesas = pokeSpecies.map((forms) =>
+                fetch(forms.url).then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                    throw new Error('Error en la conexión a la API');
+                })
+            );
+            return Promise.all(promesas);
+        })
+        .then((dataPoke) => {
+            const color = dataPoke.map((pokemon) => pokemon);
+            _pokeColor = color;
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+})
+
+boton.addEventListener('click', (e) => {
     e.preventDefault();
     //Generate random pokemon ID
     const idPokemon = parseInt(Math.random() * 151)+1;
-    //Call to the API
-    const consulta = await fetch('https://pokeapi.co/api/v2/pokemon-form/?limit=151');
-    const pokemones = await consulta.json();
-    const mostrar = pokemones.results;
-    imprimirPokemones(mostrar, idPokemon);
+    imprimirPokemones(idPokemon);
 })
         
-async function imprimirPokemones (pokemones, idPokemones){
-    //Nombre del Pokemon
-    const nombrePokemon = pokemones[idPokemones-1].name;
-    //Tipo del Pokemon
-    const tiposPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${idPokemones}/`);
-    const jsonPokemon = await tiposPokemon.json();
-    //Foto del Pokemon
-    const consultaFoto = await fetch(`https://pokeapi.co/api/v2/pokemon-form/${idPokemones}/`);
-    const grupoFoto = await consultaFoto.json();
+function imprimirPokemones (randomPokemonId){
+    const pokeNombre = pokedex[randomPokemonId].name;
+    const photoPokemon = pokePhoto[randomPokemonId];
+    const pokeTipo = pokeType[randomPokemonId]
+    const colorPokemon = _pokeColor[randomPokemonId].color.name
+    console.log(colorPokemon)
+
     //Color del Pokemon
-    const pokeColorApi = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${idPokemones}`);
-    const pokeColorGroup = await pokeColorApi.json();
-    const pokeColor = pokeColorGroup.color.name;
+
     const div = document.getElementById('poke-tarjeta');
-    
-    const pokeFoto = grupoFoto.sprites.front_default;
-    const tipoPokemon = jsonPokemon.types[0].type.name;
     
     //Add color to the poke-card background
     div.innerHTML = 
     `
-        <img src="${pokeFoto}" class="foto" alt="">
-        <p class="poke-number"><span class="fondo"><b>Id:</b> ${idPokemones}</span></p>
-        <p class="poke-tittle titulo"><span class="fondo"><b>Pokemon:</b> ${nombrePokemon}</span></p>
-        <p class="poke-type titulo"><span class="fondo"><b>Type:</b> ${tipoPokemon}</span></p>
+        <img src="${photoPokemon}" class="foto" alt="">
+        <p class="poke-number"><span class="fondo"><b>Id:</b> ${randomPokemonId + 1 }</span></p>
+        <p class="poke-tittle titulo"><span class="fondo"><b>Pokemon:</b> ${pokeNombre}</span></p>
+        <p class="poke-type titulo"><span class="fondo"><b>Type:</b> ${pokeTipo}</span></p>
     `
-    switch (pokeColor){
+   switch (colorPokemon){
         case 'black':
-                div.style.backgroundColor = '#262626';
-                break;
-            case 'blue':
-                div.style.backgroundColor = '#2196F3';
-                break;
-            case 'brown':
-                div.style.backgroundColor = '#795548';
-                break;
-            case 'gray':
-                div.style.backgroundColor = '#B2BEC3';
-                break;
-            case 'green':
-                div.style.backgroundColor = '#4E8644';
-                break;
-            case 'pink':
-                div.style.backgroundColor = '#ECB0B6';
-                break;
-            case 'purple':
-                div.style.backgroundColor = '#7E47A4';
-                break;
-            case 'red':
-                div.style.backgroundColor = '#D52625';
-                break;
-            case 'white':
-                div.style.backgroundColor = '#FCFCF5';
-                break;
-            case 'yellow':
-                div.style.backgroundColor = '#FFE164';
-                break;       
+            div.style.backgroundColor = '#262626';
+            break;
+        case 'blue':
+            div.style.backgroundColor = '#2196F3';
+            break;
+        case 'brown':
+            div.style.backgroundColor = '#795548';
+            break;
+        case 'gray':
+            div.style.backgroundColor = '#B2BEC3';
+            break;
+        case 'green':
+            div.style.backgroundColor = '#4E8644';
+            break;
+        case 'pink':
+            div.style.backgroundColor = '#ECB0B6';
+            break;
+        case 'purple':
+            div.style.backgroundColor = '#7E47A4';
+            break;
+        case 'red':
+            div.style.backgroundColor = '#D52625';
+            break;
+        case 'white':
+            div.style.backgroundColor = '#FCFCF5';
+            break;
+        case 'yellow':
+            div.style.backgroundColor = '#FFE164';
+            break;       
     }
     const contenedorPokemon = document.getElementById('div-pokemon');
     contenedorPokemon.appendChild(div);
